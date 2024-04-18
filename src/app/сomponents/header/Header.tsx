255a293045d287e59cfd4e984d3ed84f8ce69dbc
MeowSoul <agent_hammer@mail.ru>
2024-04-18 17:09:22 +0500

    Создание и стилизация компонента header

---
+++ src/app/сomponents/header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './header.module.css'
+
+const Header = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.menu}>
+                <p className={styles.menuItem}>Портфолио</p>
+                <p className={styles.menuItem}>Услуги</p>
+                <p className={styles.menuItem}>Обучение</p>
+                <p className={styles.menuItem}>Запись</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
