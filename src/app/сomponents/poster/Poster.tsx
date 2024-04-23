ceffaacc69ec9bc9d6cfd3fa9e15b42a0b3cd58e
MeowSoul <agent_hammer@mail.ru>
2024-04-23 23:02:02 +0500

    Я хз

--- src/app/сomponents/poster/Poster.tsx
+++ src/app/сomponents/poster/Poster.tsx
@@ -6,7 +6,7 @@ const backgroundImage = "/img/poster.jpg"
 const Poster = () => {
     return (
         <div style={{backgroundImage:`url(${backgroundImage})`}} className={styles.main}>
-            
+            <img src="/img/line.png" alt="line" className={styles.line}/>
         </div>
     );
 };
