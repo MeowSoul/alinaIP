7bb827f60a28ef3d421f5375867f871013922cf1
MeowSoul <agent_hammer@mail.ru>
2024-04-24 23:47:34 +0500

    Рефактор

--- src/app/сomponents/poster/Poster.tsx
+++ src/app/сomponents/poster/Poster.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import styles from './poster.module.css'
+import React from "react"
+import styles from "./poster.module.css"
 
 const backgroundImage = "/img/poster.jpg"
 
 const Poster = () => {
     return (
-        <div style={{backgroundImage:`url(${backgroundImage})`}} className={styles.main}>
-            <img src="/img/line.png" alt="line" className={styles.line}/>
+        <div style={{ backgroundImage: `url(${backgroundImage})` }} className={styles.main}>
+            <img src="/img/line.png" alt="line" className={styles.line} />
         </div>
-    );
-};
+    )
+}
 
-export default Poster;
+export default Poster
