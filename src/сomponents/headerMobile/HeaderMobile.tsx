c89bfcceee2d2d9e3cc28c81521144d049a8e50a
MeowSoul <agent_hammer@mail.ru>
2024-04-28 22:55:20 +0500

    Добавлена на мобильной версии

---
+++ src/сomponents/headerMobile/HeaderMobile.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./headerMobile.module.css"
+
+const HeaderMobile = () => {
+    return <div className={styles.main}></div>
+}
+
+export default HeaderMobile
