df8a2957dc57583da8483b0d5d9052a626616198
MeowSoul <agent_hammer@mail.ru>
2024-04-29 21:49:10 +0500

    Добавлены ссылки на страницы. Сделано бургер меню

--- src/сomponents/headerMobile/HeaderMobile.tsx
+++ src/сomponents/headerMobile/HeaderMobile.tsx
@@ -1,8 +1,38 @@
-import React from "react"
+"use client"
+
+import React, { useState } from "react"
 import styles from "./headerMobile.module.css"
+import Link from "next/link"
 
 const HeaderMobile = () => {
-    return <div className={styles.main}></div>
+    const [isActive, setActive] = useState(false)
+
+    return (
+        <div className={styles.main}>
+            <div className={isActive ? styles.wrapper_active : styles.wrapper} onClick={() => setActive(!isActive)}>
+                <Link href={""} className={styles.link}>
+                    Портфолио
+                </Link>
+                <Link href={""} className={styles.link}>
+                    Портфолио
+                </Link>
+                <Link href={""} className={styles.link}>
+                    Портфолио
+                </Link>
+                <Link href={""} className={styles.link}>
+                    Портфолио
+                </Link>
+            </div>
+            <div
+                className={isActive ? styles.button_active : styles.button}
+                onClick={() => setActive((prevState) => !prevState)}
+            >
+                <div className={isActive ? styles.line_active : styles.line}></div>
+                <div className={isActive ? styles.line_active : styles.line}></div>
+                <div className={isActive ? styles.line_active : styles.line}></div>
+            </div>
+        </div>
+    )
 }
 
 export default HeaderMobile
