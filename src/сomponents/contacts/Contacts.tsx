d9ceb998dc3a6d14bfa001b782315d7f1d22f379
MeowSoul <agent_hammer@mail.ru>
2024-04-30 22:14:28 +0500

    Рефактор

--- src/сomponents/contacts/Contacts.tsx
+++ src/сomponents/contacts/Contacts.tsx
@@ -7,12 +7,12 @@ const Contacts = () => {
             <p className={styles.title}>Контакты</p>
             <div className={styles.info}>
                 <p className={styles.subtitle}>Адреса:</p>
-                <p className={styles.discription}>г. Стерлитамак ул.Набережная 3/4</p>
-                <p className={styles.discription}>с.Б.Куганак ул.Ленина д.1 кв.67</p>
+                <p className={styles.description}>г. Стерлитамак ул.Набережная 3/4</p>
+                <p className={styles.description}>с.Б.Куганак ул.Ленина д.1 кв.67</p>
             </div>
             <div className={styles.info}>
                 <p className={styles.subtitle}>Телефон:</p>
-                <p className={styles.discription}>+7 917-359-52-37</p>
+                <p className={styles.description}>+7 917-359-52-37</p>
             </div>
         </div>
     )
