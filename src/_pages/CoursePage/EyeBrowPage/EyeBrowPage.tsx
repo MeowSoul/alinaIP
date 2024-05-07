392b2eed314f3f623a125d3db42825e06d135829
MeowSoul <agent_hammer@mail.ru>
2024-05-07 23:50:31 +0500

    Создана страница EyeBrow

---
+++ src/_pages/CoursePage/EyeBrowPage/EyeBrowPage.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./eyeBrowPage.module.css"
+import Course from "@/_pages/CoursePage/UI/Course/Course"
+
+const EyeBrowPage = () => {
+    return (
+        <div className={styles.main}>
+            <Course />
+        </div>
+    )
+}
+
+export default EyeBrowPage
